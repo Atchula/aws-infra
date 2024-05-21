91f6951930a4783e9834f589319045410dce63b3
Atchula <hanumaiah061@gmail.com>
2024-05-21 17:52:32 +0530

    route table association

--- route-associations.tf
+++ route-associations.tf
@@ -1,7 +1,7 @@
 resource "aws_route_table_association" "public" {
 
     count = length(aws_subnet.public)
-    route_table_id = aws_route_table.public
+    route_table_id = aws_route_table.public.id
     subnet_id      = aws_subnet.public[count.index].id
 
    
@@ -11,7 +11,7 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table_association" "private" {
 
     count = length(aws_subnet.private)
-    route_table_id = aws_route_table.private
+    route_table_id = aws_route_table.private.id
     subnet_id      = aws_subnet.private[count.index].id
 
    
@@ -22,7 +22,7 @@ resource "aws_route_table_association" "private" {
 resource "aws_route_table_association" "db" {
 
     count = length(aws_subnet.db)
-    route_table_id = aws_route_table.db
+    route_table_id = aws_route_table.db.id
     subnet_id      = aws_subnet.db[count.index].id
 
    
