f4d7c9151e616f69182a1ae81ae22de3b6e40b7f
Atchula <hanumaiah061@gmail.com>
2024-05-21 23:11:21 +0530

    peer

--- peering.tf
+++ peering.tf
@@ -4,4 +4,8 @@ resource "aws_vpc_peering" "peering" {
     vpc_id = aws_vpc.vpc.id
     peer_vpc_id = var.target_vpc == "" ? local.default : var.target_vpc
     auto_accept = var.target_vpc == "" ? true : false
+
+    tags = merge(var.common_tags,{
+        Name = "${local.name}-public-peering"
+})
 }
