bf9a35204016d29a05acbd33d9bb3f263429ec43
Atchula <hanumaiah061@gmail.com>
2024-05-21 19:56:02 +0530

    nat

--- routes.tf
+++ routes.tf
@@ -7,13 +7,13 @@ resource "aws_route" "public" {
 resource "aws_route" "private" {
     route_table_id = aws_route_table.private.id
     destination_cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws.aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat.id
 
 }
 
 resource "aws_route" "db" {
     route_table_id = aws_route_table.private.id
     destination_cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws.aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat.id
 
 }
