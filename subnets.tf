14174036ac04bb9c96c3c73937980ee08ba18286
Atchula <hanumaiah061@gmail.com>
2024-05-21 14:11:04 +0530

    subnet error3

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public" {
     
     count = length(var.cidr_block_public)
-    vpc_id = aws_vpc.vpc.vpc_id
+    vpc_id = aws_vpc.vpc.id
     cidr_block = var.cidr_block_public[count.index]
     availability_zone = local.az[count.index]
     map_public_ip_on_launch = true
