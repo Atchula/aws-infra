c2e319b617bd78ba1be1b6fd95138556ee59ae35
Atchula <hanumaiah061@gmail.com>
2024-05-21 23:39:55 +0530

    peer

--- db-subnets.tf
+++ db-subnets.tf
@@ -2,6 +2,6 @@ resource "aws_subnet_group" "db_subnet_group" {
 
     subnet_ids = aws_subnet.db[*].id
 
-    tags = tags = merge(var.common_tags,{
+    tags = merge(var.common_tags,{
         Name = "${local.name}-db-group"})
 }
