ed16ecc312e1f7ccf53631ff979f94948b7527f9
Atchula <hanumaiah061@gmail.com>
2024-05-21 14:07:08 +0530

    subnet error2

--- datasource.tf
+++ datasource.tf
@@ -1,6 +1,4 @@
 data "aws_availability_zones" "azs"{
-    filter {
-        region = "us-east-1"
-    }
+   state = "available"
     
 }
--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
     name = "${var.common_tags.Name}-${var.common_tags.Environment}"
     date = timestamp()
-    az = data.azs.names
+    az = data.aws_availability_zones.azs.names
     
 }
