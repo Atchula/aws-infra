4181e9646730ae964f8368f399a215fef09f7822
Atchula <hanumaiah061@gmail.com>
2024-05-21 18:11:03 +0530

    eip

--- nat.tf
+++ nat.tf
@@ -1,7 +1,7 @@
 resource "aws_nat_gateway" "nat" {
     count = length(aws_subnet.public)
-    allocation_id = aws_eip.eip[count.index]
-    subnet_id = aws_subnet.public[count.index]
+    allocation_id = aws_eip.eip[count.index].id
+    subnet_id = aws_subnet.public[count.index].id
 
     tags = merge(var.common_tags,{
          Name = "${local.name}-nat-local.az[count.index]"
