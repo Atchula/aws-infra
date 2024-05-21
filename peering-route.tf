8a1e3638b55be02b0b5eb10494c19f86566286b8
Atchula <hanumaiah061@gmail.com>
2024-05-21 23:13:06 +0530

    peer

--- peering-route.tf
+++ peering-route.tf
@@ -2,5 +2,5 @@ resource "aws_route"  "route_peering" {
     count = is_peering_require == true ? 1 : 0
     route_table_id = aws_route_table.public.id
     destination_cidr_block = aws.target_vpc == "" ? data.aws_vpc.default.cidr_block : aws.target_vpc.cidr_block
-    vpc_peering_connection_id = aws_vpc_peering.peering.id
+    vpc_peering_connection_id = aws_vpc_peering.peering[0].id
 }
