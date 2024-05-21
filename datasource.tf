49cb82d7c8bc8c614b1356b81f77f603a83bc44d
Atchula <hanumaiah061@gmail.com>
2024-05-22 00:01:37 +0530

    peer

--- datasource.tf
+++ datasource.tf
@@ -12,7 +12,7 @@ data "aws_vpc" "default" {
 data "aws_route_table"  "main"{
    vpc_id = data.aws_vpc.default.id
    filter {
-      name = "main"
+      name = "association.main"
       values = ["yes"]
    }
 
